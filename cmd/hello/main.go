64025971e2de66cd7a825a5f8e525411cee2c6ba
Amy Babay <amy.e.babay@gmail.com>
2022-03-09 23:38:05 -0500

    finishing tutorial

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "log"
 
     "rsc.io/quote"
 
@@ -9,9 +10,27 @@ import (
 )
 
 func main() {
+    log.SetPrefix("greetings: ") // set logger prefix
+    //log.SetFlags(0) // disable printing time, source file, line number info
+
     fmt.Println("Hello, World!")
     fmt.Println(quote.Go())
 
-    message := greetings.Hello("Amy")
-    fmt.Println(message)
+    fmt.Print("\nTesing with loop\n\n")
+    names := []string {"Amy", "Chris", "Elizabeth"}
+    for _, name := range names {
+        message, err := greetings.Hello(name)
+        if err != nil {
+            log.Fatal(err)
+        }
+        fmt.Println(message)
+    }
+
+    fmt.Print("\nTesing with slice\n\n")
+    messages, err := greetings.Hellos(names)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // no error, print the returned map
+    fmt.Println(messages)
 }
--- internal/greetings/greetings_test.go
+++ internal/greetings/greetings_test.go
@@ -1,12 +1,24 @@
 package greetings
 
-import "testing"
+import (
+    "testing"
+    "regexp"
+)
 
 func TestHello(t *testing.T) {
+    // Valid input; should succeed
     name := "Amy"
-    desiredOutput := "Hi, Amy. Welcome!"
-    ret := Hello(name)
-    if ret != desiredOutput {
-        t.Errorf("Hello(%v) == %v, want %v", name, ret, desiredOutput)
+    want := regexp.MustCompile(`\b`+name+`\b`)
+    msg, err := Hello(name)
+    if !want.MatchString(msg) || err != nil {
+        t.Errorf(`Hello("Amy") == %q, %v want match for %#q, nil`, msg, err, want)
+    }
+}
+
+func TestHelloEmpty(t *testing.T) {
+    // Invalid input; should return error
+    msg, err := Hello("")
+    if msg != "" || err == nil {
+        t.Errorf(`Hello("") == %q, %v, want "", error`, msg, err)
     }
 }
