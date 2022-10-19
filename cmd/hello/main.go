5c9564f7aaecf53072a66ba38ce8a3bcaa0285ab
Amy Babay <amy.e.babay@gmail.com>
2022-10-19 12:32:43 -0400

    run gofmt

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 
-    "rsc.io/quote"
+	"rsc.io/quote"
 
-    "go-experiments/greetings"
+	"go-experiments/greetings"
 )
 
 func main() {
-    log.SetPrefix("greetings: ") // set logger prefix
-    //log.SetFlags(0) // disable printing time, source file, line number info
+	log.SetPrefix("greetings: ") // set logger prefix
+	//log.SetFlags(0) // disable printing time, source file, line number info
 
-    fmt.Println("Hello, World!")
-    fmt.Println(quote.Go())
+	fmt.Println("Hello, World!")
+	fmt.Println(quote.Go())
 
-    fmt.Print("\nTesing with loop\n\n")
-    names := []string {"Amy", "Chris", "Elizabeth"}
-    for _, name := range names {
-        message, err := greetings.Hello(name)
-        if err != nil {
-            log.Fatal(err)
-        }
-        fmt.Println(message)
-    }
+	fmt.Print("\nTesing with loop\n\n")
+	names := []string{"Amy", "Chris", "Elizabeth"}
+	for _, name := range names {
+		message, err := greetings.Hello(name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(message)
+	}
 
-    fmt.Print("\nTesing with slice\n\n")
-    messages, err := greetings.Hellos(names)
-    if err != nil {
-        log.Fatal(err)
-    }
-    // no error, print the returned map
-    fmt.Println(messages)
+	fmt.Print("\nTesing with slice\n\n")
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// no error, print the returned map
+	fmt.Println(messages)
 }
--- greetings/greetings_test.go
+++ greetings/greetings_test.go
@@ -1,24 +1,24 @@
 package greetings
 
 import (
-    "testing"
-    "regexp"
+	"regexp"
+	"testing"
 )
 
 func TestHello(t *testing.T) {
-    // Valid input; should succeed
-    name := "Amy"
-    want := regexp.MustCompile(`\b`+name+`\b`)
-    msg, err := Hello(name)
-    if !want.MatchString(msg) || err != nil {
-        t.Errorf(`Hello("Amy") == %q, %v want match for %#q, nil`, msg, err, want)
-    }
+	// Valid input; should succeed
+	name := "Amy"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	msg, err := Hello(name)
+	if !want.MatchString(msg) || err != nil {
+		t.Errorf(`Hello("Amy") == %q, %v want match for %#q, nil`, msg, err, want)
+	}
 }
 
 func TestHelloEmpty(t *testing.T) {
-    // Invalid input; should return error
-    msg, err := Hello("")
-    if msg != "" || err == nil {
-        t.Errorf(`Hello("") == %q, %v, want "", error`, msg, err)
-    }
+	// Invalid input; should return error
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Errorf(`Hello("") == %q, %v, want "", error`, msg, err)
+	}
 }
